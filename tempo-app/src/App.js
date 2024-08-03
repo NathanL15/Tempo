68415d0067bd8a7f129a72847c9385070da4a296
samjian8 <samjian6289@gmail.com>
2024-08-03 11:20:32 -0400

    header component

--- tempo-app/src/App.js
+++ tempo-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={titleimg} alt="test"></img>
+        <img src={titleimg}></img>
       </header>
     </div>
   );
