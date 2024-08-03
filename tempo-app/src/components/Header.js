3248ed83472409f5eae5832598ae1cc0e4399f4f
Max Jin <maxjin6011@gmail.com>
2024-08-03 13:50:31 -0400

    test change

--- tempo-app/src/components/Header.js
+++ tempo-app/src/components/Header.js
@@ -5,6 +5,10 @@ import bannerImg from "./images/runner-title.jpg"
 const Header = () => {
     return (
         <div id="banner">
+            <div>
+            <h1>TEMPO</h1>
+            </div>
+            
             <img id="titleImg" src={bannerImg} alt="runner" />
         </div>
     );
