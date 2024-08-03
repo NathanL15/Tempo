25e90e854acb580a1f3bf2517d214a2e6506c908
Max Jin <maxjin6011@gmail.com>
2024-08-03 10:50:44 -0400

    changed port

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './account.env' });
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to my server!');
